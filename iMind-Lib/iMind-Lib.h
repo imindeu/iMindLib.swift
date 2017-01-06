29cc3b3a9c04eb0b3f07fa00448ac33a70d6ed87
Révész Ádám <reveszadam@gmail.com>
2017-01-06 11:45:11 +0100

    Created the project

---
+++ iMind-Lib/iMind-Lib.h
@@ -0,0 +1,19 @@
+//
+//  iMind-Lib.h
+//  iMind-Lib
+//
+//  Created by Révész Ádám on 2017. 01. 06..
+//  Copyright © 2017. iMind. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for iMind-Lib.
+FOUNDATION_EXPORT double iMind_LibVersionNumber;
+
+//! Project version string for iMind-Lib.
+FOUNDATION_EXPORT const unsigned char iMind_LibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iMind_Lib/PublicHeader.h>
+
+
