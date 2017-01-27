7853a06faaa48a76880fd31989bb2ba49914ce88
Révész Ádám <reveszadam@gmail.com>
2017-01-27 03:17:24 +0100

    lint fixes on string and date extensions

--- Source/Extensions/Date+Extensions.swift
+++ Source/Extensions/Date+Extensions.swift
@@ -9,82 +9,88 @@
 import Foundation
 
 extension Date {
-    struct Date {
-        static let formatterShortDate = DateFormatter(dateFormat: "MMM dd yyyy")
-    }
-    var shortDate: String {
-        return Date.formatterShortDate.string(from: self)
-    }
     
     func timeAgoSinceDate(numericDates:Bool) -> String {
         let calendar = Calendar.current
         let now = Foundation.Date()
         let earliest = (now as NSDate).earlierDate(self)
         let latest = (earliest == now) ? self : now
-        let components:DateComponents = (calendar as NSCalendar).components([NSCalendar.Unit.minute , NSCalendar.Unit.hour , NSCalendar.Unit.day , NSCalendar.Unit.weekOfYear , NSCalendar.Unit.month , NSCalendar.Unit.year , NSCalendar.Unit.second], from: earliest, to: latest, options: NSCalendar.Options())
-        guard let cYear = components.year, let cMonth = components.month, let cWeekOfYear = components.weekOfYear, let cDay = components.day, let cHour = components.hour, let cMinute = components.minute, let cSecond = components.second else {
+        let components: DateComponents =
+            (calendar as NSCalendar).components(
+                [
+                    NSCalendar.Unit.minute,
+                    NSCalendar.Unit.hour,
+                    NSCalendar.Unit.day,
+                    NSCalendar.Unit.weekOfYear,
+                    NSCalendar.Unit.month,
+                    NSCalendar.Unit.year,
+                    NSCalendar.Unit.second
+                ],
+                from: earliest, to: latest, options: NSCalendar.Options())
+        guard
+            let cYear = components.year,
+            let cMonth = components.month,
+            let cWeekOfYear = components.weekOfYear,
+            let cDay = components.day,
+            let cHour = components.hour,
+            let cMinute = components.minute,
+            let cSecond = components.second
+        else {
             return "Just now"
         }
-        if (cYear >= 2) {
+        if cYear >= 2 {
             return "\(cYear) years ago"
-        } else if (cYear >= 1){
-            if (numericDates){
+        } else if cYear >= 1 {
+            if numericDates {
                 return "1 year ago"
             } else {
                 return "Last year"
             }
-        } else if (cMonth >= 2) {
+        } else if cMonth >= 2 {
             return "\(cMonth) months ago"
-        } else if (cMonth >= 1){
-            if (numericDates){
+        } else if cMonth >= 1 {
+            if numericDates {
                 return "1 month ago"
             } else {
                 return "Last month"
             }
-        } else if (cWeekOfYear >= 2) {
+        } else if cWeekOfYear >= 2 {
             return "\(cWeekOfYear) weeks ago"
-        } else if (cWeekOfYear >= 1){
-            if (numericDates){
+        } else if cWeekOfYear >= 1 {
+            if numericDates {
                 return "1 week ago"
             } else {
                 return "Last week"
             }
-        } else if (cDay >= 2) {
+        } else if cDay >= 2 {
             return "\(cDay) days ago"
-        } else if (components.day! >= 1){
-            if (numericDates){
+        } else if components.day! >= 1 {
+            if numericDates {
                 return "1 day ago"
             } else {
                 return "Yesterday"
             }
-        } else if (cHour >= 2) {
+        } else if cHour >= 2 {
             return "\(cHour) hours ago"
-        } else if (cHour >= 1){
-            if (numericDates){
+        } else if cHour >= 1 {
+            if numericDates {
                 return "1 hour ago"
             } else {
                 return "An hour ago"
             }
-        } else if (cMinute >= 2) {
+        } else if cMinute >= 2 {
             return "\(cMinute) minutes ago"
-        } else if (cMinute >= 1){
-            if (numericDates){
+        } else if cMinute >= 1 {
+            if numericDates {
                 return "1 minute ago"
             } else {
                 return "A minute ago"
             }
-        } else if (cSecond >= 3) {
+        } else if cSecond >= 3 {
             return "\(cSecond) seconds ago"
         } else {
             return "Just now"
         }
         
-    }
-}
-
-extension DateFormatter {
-    convenience init(dateFormat: String) {
-        self.init()
-        self.dateFormat = dateFormat
     }
 }
--- Source/Extensions/String+Extensions.swift
+++ Source/Extensions/String+Extensions.swift
@@ -8,13 +8,16 @@
 #if !os(macOS) && !os(Linux)
 
 import Foundation
-import UIKit
 
 extension String {
     
     func isValidEmail() -> Bool {
         do {
-            let regex = try NSRegularExpression(pattern: "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$", options: .caseInsensitive)
+            let regex = try NSRegularExpression(
+                pattern: "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}" +
+                    "[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$",
+                options: .caseInsensitive
+            )
             return regex.firstMatch(in: self, options: NSRegularExpression.MatchingOptions(rawValue: 0), range: NSMakeRange(0, self.characters.count)) != nil
         } catch {
             return false
@@ -23,32 +26,6 @@ extension String {
     
     func isValidPassword() -> Bool {
         return self.trim().characters.count >= 6
-    }
-    
-    func attributedStringForTags(_ fontName:String, fontSize:CGFloat, fontColor:UIColor) -> NSMutableAttributedString {
-        var mutableString = NSMutableAttributedString()
-        mutableString = NSMutableAttributedString(string: self, attributes: [NSFontAttributeName:UIFont(name: fontName, size: fontSize)!])
-        
-        let words = self.components(separatedBy: " ")
-        for word in words {
-            if word.hasPrefix("#") {
-                let range = (self as NSString).range(of: word)
-                mutableString.addAttribute(NSForegroundColorAttributeName, value: fontColor, range: range)
-            }
-        }
-        
-        return mutableString
-    }
-    
-    func htmlFormattedString() -> NSMutableAttributedString? {
-        if let htmlData = "<span style='font-family: Montserrat-Regular; color: rgba(0,0,0,0.78); font-size: 14'>\(self)</span>".data(using: .utf8, allowLossyConversion: true) {
-            do {
-                return try NSMutableAttributedString(data: htmlData, options: [NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType, NSCharacterEncodingDocumentAttribute: String.Encoding.utf8.rawValue], documentAttributes: nil)
-            } catch let e as NSError {
-                print("Couldn't translate \(self): \(e.localizedDescription)")
-            }
-        }
-        return nil
     }
     
     func trim() -> String {
